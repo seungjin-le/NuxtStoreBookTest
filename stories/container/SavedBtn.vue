07aff3b8795231ef409a46e59f759d24cd821249
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-01 23:09:37 +0900

    Test

---
+++ stories/container/SavedBtn.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: false,
+    default: '저장'
+  },
+  onClick: {
+    type: Function,
+    required: false,
+    default: () => {}
+  }
+})
+</script>
+
+<template>
+  <button class="bg-[red]" @click.stop="onClick">{{ text }}</button>
+</template>
