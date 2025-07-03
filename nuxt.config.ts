bc486c439d41fc6d266c2a391c6c9c258f51ed36
bibibig <dltmdwls154@gmail.com>
2025-07-03 22:29:33 +0900

    Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   alias: {
-    '@': '/src'
+    '@': '/src',
+    components: '/components'
   }
 })
