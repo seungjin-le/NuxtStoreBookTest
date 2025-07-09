c8d62cd0619514972217b3ceed24bb586d3aa5a6
bibibig <dltmdwls154@gmail.com>
2025-07-09 22:26:11 +0900

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <script setup>
 const test = ref('')
+const inputVal = ref('')
 </script>
 
 <template>
   <div>
     <input v-model="test" type="text" />
+    <input v-model="inputVal" type="text" />
   </div>
 </template>
