bd644e2640f013f54abb7e2c04795acd1d8d5aad
bibibig <dltmdwls154@gmail.com>
2025-10-18 23:56:15 +0900

    test

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,9 +3,7 @@ import DefaultBtn from 'components/buttons/DefaultBtn.vue'
 </script>
 
 <template>
-  <div
-    class="bg-[gray] size-full min-h-screen flex-col-center-start [&_input]:border-1 [&_input]:border-[blue] flex-col-center gap-[20px]"
-  >
+  <div class="">
     <input />
     <DefaultBtn v-bind="{ text: 'test', onClick: () => console.log('test') }" />
   </div>
